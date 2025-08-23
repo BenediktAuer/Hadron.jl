4853184a44a6e1116b07d493c0b903189519b2d9
BenediktAuer <benedikt_auer01@web.de>
2025-08-23 19:13:43 +0200

    [Add] -errorbars -fixes -scatter

--- ext/MakieExtension.jl
+++ ext/MakieExtension.jl
@@ -8,32 +8,61 @@ end
 
 using Hadron
 import Hadron: boothist,boothist!, BootstrapResult
+import Statistics: std
+" QQNORM conversion"
 
-
-
-Makie.convert_arguments(P::Type{<:QQNorm}, m::BootstrapResult; qqline=:none, col=1)= return Makie.convert_arguments(P, m[col]; qqline=qqline)
+Makie.convert_arguments(P::Type{<:QQNorm}, m::BootstrapResult; qqline=:none, col=1)=  Makie.convert_arguments(P, m[col]; qqline=qqline)
 Makie.used_attributes(::Type{<:QQNorm},m::BootstrapResult ) = (:qqline,:col)
 
-# @recipe Bootqq begin
-#     qqline=:fit
-#     Makie.mixin_generic_plot_attributes()...
-# end
-# function Makie.plot!(plt::QQNorm{<:Tuple{BootstrapResult}}) 
-# input_nodes = [:converted_1, :col, :qqline]
-# output_nodes = [:data, :qqline]
-# map!(plt.attributes, input_nodes,output_nodes) do data, col, qqline
+" Errorbars conversion"
+Makie.convert_arguments(P::Type{<:Errorbars},x::Vector, m::Vector{BootstrapResult}; col=1) = Makie.convert_arguments(P,x,[getfield(k,:observable)[col] for k in m], [std(k[col]) for k in m ] )
+Makie.convert_arguments(P::Type{<:Errorbars}, x::Real, m::BootstrapResult; col=1) = Makie.convert_arguments(P,[x],[getfield(m,:observable)[col]], [std(m[col])])
+Makie.used_attributes(::Type{<:Errorbars},x,m::Union{BootstrapResult, Vector{BootstrapResult}}) = (:col,)
 
-#     return (data[col],qqline)
-# end
-#     qqnorm!(plt,plt.attributes, plt.data; qqline=plt.qqline)
-# end
-# Makie.convert_singe_argument(m::BootstrapResult,i::Int=1) = m[i]
+Makie.convert_arguments(P::Type{<:Errorbars}, m::Union{BootstrapResult, Vector{BootstrapResult}},x::Vector; col=1) = Makie.convert_arguments(P,[getfield(k,:observable)[col] for k in m],x, [std(k[col]) for k in m ]; direction = :x )
+function Makie.convert_arguments(P::Type{<:Errorbars}, m::BootstrapResult, x::Real; col=1) 
+    return Makie.convert_arguments(P,[getfield(m,:observable)[col]],[x], [std(m[col])])
+end
+Makie.used_attributes(::Type{<:Errorbars},m::Union{BootstrapResult, Vector{BootstrapResult}},x) = (:col, )
+
+
+" Scatter conversion"
+Makie.convert_arguments(P::Type{<:Scatter}, x::Vector, m::Vector{BootstrapResult}; col=1  ) = Makie.convert_arguments(P, x,[getfield(k,:observable)[col] for k in m] )
+Makie.convert_arguments(P::Type{<:Scatter}, x::Real, m::BootstrapResult; col=1) = Makie.convert_arguments(P,[x],[getfield(m,:observable)[col]])
+Makie.used_attributes(::Type{<:Scatter},x,m::Union{BootstrapResult, Vector{BootstrapResult}}) = (:col,)
+
+Makie.convert_arguments(P::Type{<:Scatter}, m::Vector{BootstrapResult}, x::Vector; col=1  ) = Makie.convert_arguments(P, [getfield(k,:observable)[col] for k in m],x )
+Makie.convert_arguments(P::Type{<:Scatter}, m::BootstrapResult, x::Real; col=1) = Makie.convert_arguments(P,[getfield(m,:observable)[col]],[x])
+Makie.used_attributes(::Type{<:Scatter},m::Union{BootstrapResult, Vector{BootstrapResult}},x) = (:col, )
+"""
+    boothist(bootstrap)
+
+Plot a histogram of `bootsrap` centered around the observable idicated by a vertical line 
+
+"""
 @recipe BootHist  begin 
-
+"""
+Number of bins left and right of the value of the observable
+"""
         nbins = 5        # number of bins
-        color = :steelblue    # histogram color
-        linecolor = :red      # observable line color
+
+        """
+    Color can either be:
+    * a vector of `bins` colors
+    * a single color
+    * `:values`, to color the bars with the values from the histogram
+        """
+        color = :steelblue 
+        """
+        Color of the vertical line
+        """
+        linecolor = :red     
         linewidth = 2
+
+        """
+        If `bootstrap` has multiple observables, `col` selects which observables to plot.
+        Type `Int`
+        """
         col = 1
 
         Makie.mixin_generic_plot_attributes()...
@@ -62,6 +91,9 @@ function Hadron.analyse(m::BootstrapResult, ;col=1, QQmarkercolor=Makie.wong_col
     fig = Figure()
     a1 = Axis(fig[1,1], title = "Histogram",xlabel=String(getfield(m,:f)[col]) ,ylabel="Hits") 
     a2 = Axis(fig[1,2], title = "Q-Q Plot - $(String(getfield(m,:f)[col]))")
+    Label(fig[1, 1:2, Top()], " ", valign = :top,
+    font = :bold,
+    padding = (5, 5, 20, 5))
     boothist!(a1,m,col=col,kw_args... )
     qqnorm!(a2,m,col=col,markercolor=QQmarkercolor, color=QQcolor, qqline=:fit)
     return fig
